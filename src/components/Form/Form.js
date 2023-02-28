6911ade47849fbf770fdec5dd880b884442629e8
Maksim1903 <dovgusha.m@gmail.com>
2023-02-28 17:52:08 +0100

    .

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import { ContactForm, Label, Span, AddContactButton } from './Form.styled';
 import PropTypes from 'prop-types';
-const Form = ({ name, number, addContacts, handleInputChange }) => {
+const Form = ({ addContacts, handleInputChange }) => {
   return (
     <ContactForm onSubmit={addContacts}>
       <Label htmlFor="name">
@@ -8,7 +8,6 @@ const Form = ({ name, number, addContacts, handleInputChange }) => {
         <input
           type="text"
           name="name"
-          value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           onChange={handleInputChange}
@@ -20,7 +19,6 @@ const Form = ({ name, number, addContacts, handleInputChange }) => {
         <input
           type="tel"
           name="number"
-          value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           onChange={handleInputChange}
@@ -34,8 +32,6 @@ const Form = ({ name, number, addContacts, handleInputChange }) => {
 export default Form;
 
 Form.propTypes = {
-  name: PropTypes.string,
-  number: PropTypes.string,
   addContacts: PropTypes.func,
   handleInputChange: PropTypes.func,
 };
